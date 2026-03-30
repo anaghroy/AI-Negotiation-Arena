e7add17766d7f7b651f23a532b5e39b94c7a9432
AnaghRoy <royanagh1999@gmail.com>
2026-03-30 12:46:19 +0530

    update dist

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,9 +4,11 @@ import negotiationRoutes from "./routes/negotiation.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cookieParser());
 app.use(
@@ -21,12 +23,10 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/negotiation", negotiationRoutes);
 
-app.use(express.static(path.join(__dirname, "dist")));
-app.get("/", (req, res) => {
-  res.send("AI Negotiation Arena Backend Running");
-});
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get((req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
 export default app;
